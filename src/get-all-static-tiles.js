2e49a84ade1ac68dcef276e190ed268b76ea2a03
Josh Erb <josh.erb@mapbox.com>
2020-04-29 11:23:03 -0400

    clean map of tiles

--- src/get-all-static-tiles.js
+++ src/get-all-static-tiles.js
@@ -5,16 +5,16 @@ const getStaticTile = require('./get-static-tile');
 
 function getAllTiles(tileImgProperties, { style, accessToken }) {
   const tiles = tileImgProperties.tiles;
-  const promises = [];
-  tiles.forEach((tile) => {
+
+  const retrievedTiles = tiles.map((tile) => {
     const tileUrl = getTileUrl({ style, accessToken }, tile.z, tile.x, tile.y);
     // ensure our pixel offsets are associated with the tiles so we can
     // properly blend them when stitching tiles later
     const { px, py } = tile;
-    promises.push(getStaticTile(tileUrl, { x: px, y: py }));
+    return getStaticTile(tileUrl, { x: px, y: py });
   });
 
-  return Promise.all(promises).catch((e) => {
+  return Promise.all(retrievedTiles).catch((e) => {
     throw new Error(e);
   });
 }
